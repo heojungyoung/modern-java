8c1dd461291bdc8f324746d1b9343505d3204823
tyireal <tyireal@morusoft.com>
2022-06-03 16:41:39 +0900

    commit

--- src/main/java/com/bingbingpa/ch06/Grouping.java
+++ src/main/java/com/bingbingpa/ch06/Grouping.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 
 import static com.bingbingpa.ch06.Dish.dishTags;
 import static com.bingbingpa.ch06.Dish.menu;
@@ -24,6 +25,20 @@ public class Grouping {
         System.out.println("Sum calories by type: " + sumCaloriesByType());
         System.out.println("Caloric levels by type: " + caloricLevelsByType());
     }
+
+    /*
+        Dishes grouped by type: {OTHER=[french fries, rice, season fruit, pizza], FISH=[prawns, salmon], MEAT=[pork, beef, chicken]}
+        Dish names grouped by type: {OTHER=[french fries, rice, season fruit, pizza], FISH=[prawns, salmon], MEAT=[pork, beef, chicken]}
+        Dish tags grouped by type: {OTHER=[salty, greasy, natural, light, tasty, fresh, fried], FISH=[roasted, tasty, fresh, delicious], MEAT=[salty, greasy, roasted, fried, crisp]}
+        Caloric dishes grouped by type: {OTHER=[french fries, pizza], FISH=[], MEAT=[pork, beef]}
+        Dishes grouped by caloric level: {DIET=[chicken, rice, season fruit, prawns], NORMAL=[beef, french fries, pizza, salmon], FAT=[pork]}
+        Dishes grouped by type and caloric level: {OTHER={DIET=[rice, season fruit], NORMAL=[french fries, pizza]}, FISH={DIET=[prawns], NORMAL=[salmon]}, MEAT={DIET=[chicken], NORMAL=[beef], FAT=[pork]}}
+        Count dishes in groups: {OTHER=4, FISH=2, MEAT=3}
+        Most caloric dishes by type: {OTHER=Optional[pizza], FISH=Optional[salmon], MEAT=Optional[pork]}
+        Most caloric dishes by type: {OTHER=pizza, FISH=salmon, MEAT=pork}
+        Sum calories by type: {OTHER=1550, FISH=850, MEAT=1900}
+        Caloric levels by type: {OTHER=[DIET, NORMAL], FISH=[DIET, NORMAL], MEAT=[DIET, NORMAL, FAT]}
+    */
 
     private static Map<Dish.Type, List<Dish>> groupDishesByType() {
         return menu.stream().collect(groupingBy(Dish::getType));
@@ -92,10 +107,7 @@ public class Grouping {
 
     private static Map<Dish.Type, Dish> mostCaloricDishesByTypeWithoutOptionals() {
         return menu.stream().collect(
-                groupingBy(Dish::getType,
-                        collectingAndThen(
-                                reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2),
-                                Optional::get)));
+                toMap(Dish::getType, Function.identity(), (d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2));
     }
 
     private static Map<Dish.Type, Integer> sumCaloriesByType() {
